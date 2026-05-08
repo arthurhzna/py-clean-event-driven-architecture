57e132b7cc67057008fb033996e26e094e55a3b5
arthurhzna <arthurhozana123@gmail.com>
2026-05-08 17:03:13 +0700

    fix something

--- infrastructure/bootstrap/event_bus.py
+++ infrastructure/bootstrap/event_bus.py
@@ -1,8 +1,10 @@
 from domain.events import (
     DeviceOnlineEvent,
+    PersonDisappearedEvent
 )
 from infrastructure.event_handlers.redis_publish_handler import (
     RedisPublishHandler,
+    PersonDisappearedHandler
 )
 
 from domain.interface.messaging.event_bus import BaseEventBus
@@ -13,6 +15,13 @@ def register_events(
 ) -> None:
     event_bus.subscribe(
         DeviceOnlineEvent,
-        RedisPublishHandler(),
+        RedisPublishHandler(),  
     )
 
+def person_disappeared_event(
+    event_bus: BaseEventBus,
+) -> None:
+    event_bus.subscribe(
+        PersonDisappearedEvent,
+        PersonDisappearedHandler(),
+    )
--- infrastructure/bootstrap/usecases.py
+++ infrastructure/bootstrap/usecases.py
@@ -1,11 +1,23 @@
 # infrastructure/bootstrap/usecases.py
 
-from application.usecases import (
+from application.usecase.create_order_usecase import (
     CreateOrderUseCase,
+)
+from application.usecase.mark_person_disappeared_usecase import (
+    MarkPersonDisappearedUseCase,
+)
+from application.usecase.register_device_usecase import (
+    RegisterDeviceUseCase,
+)
+from application.state.state_manager import (
+    StateManager,
 )
 
 from infrastructure.bootstrap.services import (
+    build_device_repository,
+    build_person_repository,
     build_pricing_service,
+    build_state_manager,
 )
 
 
@@ -15,4 +27,26 @@ def build_create_order_usecase():
 
     return CreateOrderUseCase(
         pricing_service=pricing_service,
+    )
+
+
+def build_mark_person_disappeared_usecase():
+
+    person_repository = build_person_repository()
+
+    return MarkPersonDisappearedUseCase(
+        PersonRepository=person_repository,
+    )
+
+
+def build_register_device_usecase(
+    state_manager: StateManager | None = None,
+):
+
+    sm = state_manager or build_state_manager()
+    device_repository = build_device_repository()
+
+    return RegisterDeviceUseCase(
+        state_manager=sm,
+        device_repository=device_repository,
     )
